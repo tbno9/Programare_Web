e528190ab1a246fe8eb282a475fdaef391a64615
Bogdana Tinc <tincbogdananelia@yahoo.com>
2026-05-13 23:41:28 +0300

    update PROIECT

--- PROIECT/db/users.js
+++
@@ -1,9 +0,0 @@
-const users = [
-    {
-        email: "admin@boco.ro",
-        // Acesta este hash-ul pentru textul "parola123"
-        password: "$2b$10$7pS6ZfU1FzD3m9N7kR8uO.nK1E9rGZ6Q5W6XyYvVbBcCdDeEfFgGh" 
-    }
-];
-
-module.exports = users;
